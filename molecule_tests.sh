bb1ddc18c710e430e4eaea8d119ad6743e5a40c0
Michael Buluma <buluma@users.noreply.github.com>
2022-07-08 19:47:21 +0300

    Use TOX on GitLab.

---
+++ molecule_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+
+MOLECULE_DISTROS="centos7 centos8 ubuntu1604 ubuntu1804	ubuntu2004 ubuntu2104 ubuntu2204"
+
+for MOLECULE_DISTRO in $MOLECULE_DISTROS; do
+  echo "*** $MOLECULE_DISTRO"
+  export MOLECULE_DISTRO
+  # molecule test
+  if [ -f tox.ini ] ; then tox ; fi
+  if [ ! -f tox.ini ] ; then molecule test ; fi
+done
